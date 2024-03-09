a62af98b83bcea8c702ae2c40c93bf1384e83c9a
DimaYermolenkoOut <dima.yermolenko@outlook.com>
2024-03-09 22:10:30 +0200

    registartions user work

---
+++ authentication/models.py
@@ -0,0 +1,64 @@
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError("Users must have a username")
+        if email is None:
+            raise TypeError("Users must have an email address")
+
+        user = self.model(email=self.normalize_email(email), username=username)
+
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        if password is None:
+            raise TypeError("Superusers must have a password.")
+        if email is None:
+            raise TypeError("Superusers must have an email.")
+
+        user = self.create_user(username, email, password, **extra_fields)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+    # def create_superuser(self, username, email, password=None, **extra_fields):
+    #     if password is None:
+    #         raise TypeError("Superusers must have a password.")
+    #     if email is None:
+    #         raise TypeError("Superusers must have an email.")
+
+    #     extra_fields.setdefault('is_staff', True)
+    #     extra_fields.setdefault('is_superuser', True)
+
+    #     return self.create_user(username, email, password, **extra_fields)
+
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    is_verifild = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    def tokens(self):
+        return ""
+    
+
+
