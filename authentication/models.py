babd839a0be1d072dc0adccd32f67f2ca3a84912
DimaYermolenkoOut <dima.yermolenko@outlook.com>
2024-03-13 18:44:59 +0200

    swagger test by swagger verifyEmail work

--- authentication/models.py
+++ authentication/models.py
@@ -43,7 +43,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=True)
     phone = models.CharField(max_length=255, null=True, blank=True)
-    is_verifild = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
