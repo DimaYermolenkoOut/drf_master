fb0269ce52deefd34d3479503b515b8fd1013c9d
DimaYermolenkoOut <dima.yermolenko@outlook.com>
2024-03-14 20:44:49 +0200

    змінив permitions

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from authentication import models
 
-# Register your models here.
+
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'email', 'username']
