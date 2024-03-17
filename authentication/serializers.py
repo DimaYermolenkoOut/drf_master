30191b33c6eadc1a7140f2f53ba367ed6d6f5994
DimaYermolenkoOut <dima.yermolenko@outlook.com>
2024-03-17 21:44:38 +0200

    reset password complete

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,9 +1,15 @@
+from django.contrib.sites.shortcuts import get_current_site
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import smart_str, smart_bytes, force_str, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 from .models import User
+from .utils import Util
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -73,3 +79,40 @@ class LoginSerializer(serializers.ModelSerializer):
         }
 
 
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField(min_length=2)
+
+    class Meta:
+        fields = ['email']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+
+        return super().validate(attrs)
+
+
+class SetNewPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(min_length=6, max_length=68, write_only=True)
+    token = serializers.CharField(min_length=1, write_only=True)
+    uidb64 = serializers.CharField(min_length=1, write_only=True)
+
+    class Meta:
+        fields = ['password', 'token', 'uidb64']
+
+    def validate(self, attrs):
+        try:
+            password = attrs.get('password')
+            token = attrs.get('token')
+            uidb64 = attrs.get('uidb64')
+
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=id)
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise AuthenticationFailed('The reset link is invalid', 401)
+
+            user.set_password(password)
+            user.save()
+
+            return user
+        except Exception as identifier:
+            raise AuthenticationFailed('The reset link is invalid', 401)
--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('verify-email/', views.VerifyEmail.as_view(), name='email-verify'),
+
+    path('request-reset-email/', views.RequestPasswordResetEmailView.as_view(), name='request-reset-email'),
+    path('password-reset-confirm/<uidb64>/<token>/', views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('password-reset-complete/', views.SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
 ]
