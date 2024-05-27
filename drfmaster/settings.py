c3edbbdafc90ed1375a7f8c85bf7fb4872826a3b
DimaYermolenkoOut <dima.yermolenko@outlook.com>
2024-05-27 20:28:18 +0300

    github actions

--- drfmaster/settings.py
+++ drfmaster/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import datetime
 from pathlib import Path
+
+import dj_database_url
 from dotenv import load_dotenv
 import os
 
@@ -124,10 +126,10 @@ GRAPHENE = {
 #     }
 # }
 # цей код працює
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=f"postgres://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:5432/{os.getenv('POSTGRES_DB')}")
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"postgres://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:5432/{os.getenv('POSTGRES_DB')}")
+}
 # цей код краще для doker
 # DATABASES = {
 #     'default': {
@@ -143,16 +145,16 @@ GRAPHENE = {
 # }
 
 # for mysql
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DATABASE'),
-        'USER': os.getenv('MYSQL_USER'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': os.getenv('MYSQL_DB_HOST'),
-        'PORT': os.getenv('MYSQL_DB_PORT', '3306'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv('MYSQL_DATABASE'),
+#         'USER': os.getenv('MYSQL_USER'),
+#         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+#         'HOST': os.getenv('MYSQL_DB_HOST'),
+#         'PORT': os.getenv('MYSQL_DB_PORT', '3306'),
+#     }
+# }
 # Настройки для SILENCED_SYSTEM_CHECKS
 SILENCED_SYSTEM_CHECKS = ["models.W036"]
 # Password validation
